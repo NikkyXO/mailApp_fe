4e1207f00da9f8f3e0e6976186144a57dc340e93
Olanike <alexisdame2017@gmail.com>
2024-12-03 04:37:20 +0100

    add api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ console.log("baseUrl", import.meta.env.VITE_API_URL);
 
 
 const api = axios.create({   
-    baseURL: import.meta.env.VITE_API_URL,     
+    baseURL: import.meta.env.VITE_API_URL || 'https://mail-app-be.vercel.app/',     
     headers: {       
       'Content-Type': 'application/json',
       'Accept': 'application/json'
